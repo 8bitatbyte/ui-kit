f01ce44419d56f67572ba1fd831987ea14bf5943
Artem Bashkatov <adaturing@yandex.ru>
2022-12-12 16:53:58 +0300

    added new component: Loader

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Loader } from './components/ui/loader';
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Loader />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ui/loader/index.tsx
@@ -0,0 +1,8 @@
+import './style.css';
+
+export const Loader = () => {
+    return <div className="spinner">
+        <div className="loader inner"></div>
+        <div className="loader outer"></div>
+    </div>
+}
